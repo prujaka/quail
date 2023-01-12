55b5810614aff8b8d7a71a3be9ffd40cfaf582ec
Sergey Tkachenko <sergeyprujina@gmail.com>
2023-01-12 13:46:34 +0100

    added empty shallowwater package with the header and a couple of imports

---
+++ src/physics/shallowwater/shallowwater.py
@@ -0,0 +1,39 @@
+# ------------------------------------------------------------------------ #
+#
+#       quail: A lightweight discontinuous Galerkin code for
+#              teaching and prototyping
+#		<https://github.com/IhmeGroup/quail>
+#
+#		Copyright (C) 2020-2021
+#
+#       This program is distributed under the terms of the GNU
+#		General Public License v3.0. You should have received a copy
+#       of the GNU General Public License along with this program.
+#		If not, see <https://www.gnu.org/licenses/>.
+#
+# ------------------------------------------------------------------------ #
+
+# ------------------------------------------------------------------------ #
+#
+#       File : src/physics/shallowwater/shallowwater.py
+#
+#       Contains class definitions for 1D and 2D hyperbolic
+#       shallow water equations.
+#
+#       By Sergey Tkachenko (https://github.com/prujaka).
+#
+# ------------------------------------------------------------------------ #
+
+from enum import Enum
+import numpy as np
+
+import errors
+import general
+
+import physics.base.base as base
+import physics.base.functions as base_fcns
+from physics.base.functions import BCType as base_BC_type
+from physics.base.functions import ConvNumFluxType as base_conv_num_flux_type
+from physics.base.functions import FcnType as base_fcn_type
+
+# TODO: paste euler-specific imports and rewrite
