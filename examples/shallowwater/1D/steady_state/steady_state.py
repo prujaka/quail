2f9429699c23c12ea9f12c335ac6de45a65d2ad7
Sergey Tkachenko <sergeyprujina@gmail.com>
2023-01-25 13:16:11 +0100

    added steady_state 1D test

---
+++ examples/shallowwater/1D/steady_state/steady_state.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+TimeStepping = {
+	"InitialTime" : 0.,
+	"FinalTime" : 1.0,
+	"CFL" : 0.1,
+	"TimeStepper" : "SSPRK3",
+}
+
+Numerics = {
+	"SolutionOrder" : 1,
+	"SolutionBasis" : "LagrangeSeg",
+	"Solver" : "DG",
+	"L2InitialCondition" : False,
+	"ApplyLimiters" : "PositivityPreserving",
+}
+
+Output = {
+	"AutoPostProcess" : False,
+}
+
+Mesh = {
+	"File" : None,
+	"ElementShape" : "Segment",
+	"NumElemsX" : 100,
+	"xmin" : 0.,
+	"xmax" : 1.,
+}
+
+Physics = {
+	"Type" : "ShallowWater",
+	"ConvFluxNumerical" : "LaxFriedrichs",
+	"GravitationalAcceleration" : 9.81,
+}
+
+InitialCondition = {
+	"Function" : "SteadyState",
+}
+
+ExactSolution = InitialCondition.copy()
+
+BoundaryConditions = {
+	"x1" : {
+		"BCType" : "SlipWall"
+		},
+	"x2" : {
+		"BCType" : "SlipWall"
+		}
+}
